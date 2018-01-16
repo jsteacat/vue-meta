48e55aea5666ebce58bdd398461a126bec7d9298
jsteacat <ubscoder@gmail.com>
2018-01-16 21:36:39 +0300

    add routing && dynamically changed meta & title

---
+++ src/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home-view">
+    <h1>Home View</h1>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+      <li><router-link to="/about/nested">Nested Route</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home-view',
+}
+</script>
---
+++ src/Nested.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="nested-view">
+    <h1>Nested View</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'nested-view',
+}
+</script>
